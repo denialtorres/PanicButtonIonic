fc8e98f685c36daeefbb21db376cbe6c757db08a
Daniel Torres <denial.torres@gmail.com>
2017-09-18 17:23:57 -0500

    login via uber

--- www/js/app.js
+++ www/js/app.js
@@ -25,11 +25,19 @@ var app = angular.module('starter', ['ionic'])
   console.log("estas en starter")
 })
 
-app.controller('saveTheWorldController', function($scope){
+app.controller('saveTheWorldController', function($scope, $http){
 
  $scope.saveTheWorld = function(){
    console.log('press button')
    $scope.buttonMessage = "World is saved"
+
+   var url = 'https://api.uber.com/v1/products?latitude=37.7759792&longitude=-122.41823';
+   var xhr = new XMLHttpRequest();
+   xhr.open('GET', url);
+   xhr.setRequestHeader("Authorization", "Token ikGvlAJSejPSY6bUp7APhxkwyu5ermguZnreUaCd");
+   xhr.send();
+   window.location="https://login.uber.com/oauth/v2/authorize?response_type=code&client_id=_g8Fw1o5KsrVRNBNskv2cMEMhoE6RPbW";
+
  };
 
 });
