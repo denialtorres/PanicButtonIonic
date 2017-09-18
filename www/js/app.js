beb041338f73da61562e46ccec353a6b7c80d077
Daniel Torres <denial.torres@gmail.com>
2017-09-18 16:45:06 -0500

    first init, button test

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -21,4 +21,15 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+
+  console.log("estas en starter")
 })
+
+app.controller('saveTheWorldController', function($scope){
+
+ $scope.saveTheWorld = function(){
+   console.log('press button')
+   $scope.buttonMessage = "World is saved"
+ };
+
+});
